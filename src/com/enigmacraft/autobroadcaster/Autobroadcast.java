ecf4cabe7e3b35cf0129f5add2886b0ff41ff1bf
Rashi <roshkins@gmail.com>
2013-01-13 14:18:59 -0800

    Added project files and basic Autobroadcast class

---
+++ src/com/enigmacraft/autobroadcaster/Autobroadcast.java
@@ -0,0 +1,15 @@
+package com.enigmacraft.autobroadcaster;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Autobroadcast extends JavaPlugin {
+
+	@Override
+	public void onEnable()
+	{
+		//TODO stuff when plugin is enabled.
+	}
+	public void onDisable()
+	{
+		//TODO stuff when plugin is disabled.
+	}
+}
